eaf29a7e2c623e7885f44462756b3c283fbad52c
Oceanicshadow88 <oceanicshadow88@gmail.com>
2025-10-09 09:01:51 +1100

    added postman call and fix warning

--- src/app/config/app.ts
+++ src/app/config/app.ts
@@ -2,7 +2,7 @@
 const dotenv = require('dotenv');
 process.env.NODE_ENV = process.env.NODE_ENV ?? 'development';
 dotenv.config();
-
+console.log(process.env.DB_CONNECTION);
 export const config = {
   environment: process.env.ENVIRONMENT ?? 'production',
   name: process.env.NAME ?? 'techscrumapp',
@@ -15,7 +15,7 @@ export const config = {
   forgotSecret: process.env.FORGET_SECRET ?? '321654',
   accessSecret: process.env.ACCESS_SECRET ?? '',
   //---------------------------v2--------------------------
-  dbConnection: process.env.TENANTS_CONNECTION ?? '',
+  dbConnection: process.env.DB_CONNECTION ?? '',
   mainDomain: process.env.MAIN_DOMAIN ?? null,
 };
 
